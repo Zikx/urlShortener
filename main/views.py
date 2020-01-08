01c59950a6718696d6443cc1c729de00d394f81f
zikx <8687706@gmail.com>
2020-01-08 17:08:34 +0900

    change

--- main/views.py
+++ main/views.py
@@ -16,11 +16,12 @@ def Make(request):
     if request.method == 'POST':
         if form.is_valid():
             NewUrl = form.save(commit=False)
-            a = shortener().issue_token() 
-            NewUrl.short_url = a
-            NewUrl.save()    
+            rtnData = shortener().issue_token() 
+            NewUrl.short_url = rtnData
+            NewUrl.save()
+            # rtnData = "https://127.0.0.1:8000/" + rtnData
         else:
             form = UrlForm()
             rtnData = short_url.objects.get(long_url=formInputData)
-    
-    return render(request, 'home.html', {'form': form, 'rtnData': rtnData})
+            
+    return render(request, 'index.html', {'form': form, 'rtnData': rtnData})
--- mainPrj/settings.py
+++ mainPrj/settings.py
@@ -118,4 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+# STATICFIELS_DIRS = {
+#     os.path.join(DEVELOPMENT_DIR, ),
+#     os.path.join(BASE_DIR, 'main/app/static')
+# }
